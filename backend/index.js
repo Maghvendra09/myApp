c2af49ee629fd41b4a5dd103b701f8c0e5ac9788
Maghvendra09 <156436102+Maghvendra09@users.noreply.github.com>
2024-03-22 09:37:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:5173", "https://finedinee.vercel.app"],
     methods: ["POST"],
-    credentials: true,
+    // credentials: true,
   })
 );
 
