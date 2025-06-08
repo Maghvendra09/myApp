dde7abdfabafe68a199e5ae5c146069e19c9a0a4
Maghvendra09 <156436102+Maghvendra09@users.noreply.github.com>
2025-06-08 11:03:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,12 @@ console.log("PORT:", process.env.PORT);
 
 app.use(
   cors({
-    origin: "*",
-    methods: ["POST"],
+    origin: ["https://finedinee.vercel.app"], 
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
