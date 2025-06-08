ad4ea7f94846b9fd867de65ac0f01719fd0f204c
Maghvendra09 <156436102+Maghvendra09@users.noreply.github.com>
2025-06-08 11:13:26 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,18 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
-dotenv.config();
 
-
-export const dbConnection = () => {
-  // console.log(process.env.MONGO_URI);
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "RESERVATIONS",
-    })
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log(`Some error occured while connecing to database: ${err}`);
-    });
+export const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Database connected!");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    throw error; 
+  }
 };
